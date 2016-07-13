3e2323f5612b378c1c9636bb258a233e0e2c6de7
Kiorabree <rharper01@bellarmine.edu>
2016-07-13 15:10:18 -0400

    Working on Node

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+var http = require("http");
+http.createServer(function (request, response) {
+  response.writeHead(200,{'Content-Type': 'text/plain'});
+  response.end("Hello");
+}).listen(3000);
+console.log('Server running at http//');
 
+//function printMessage(username) {
+//  var message = "Welcome, " + username + ". Let's dance!";
+//  console.log(message); }
+
+//var request = http.get("")
+
+//printMessage("Kiorabree");
