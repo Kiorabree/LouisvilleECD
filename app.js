2e4a32b862c6f1001395d5894e7b3d6623cf7ee1
Kiorabree <rharper01@bellarmine.edu>
2016-07-14 01:28:49 -0400

    Added more node

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var http = require("http");
 http.createServer(function (request, response) {
-  response.writeHead(200,{'Content-Type': 'text/plain'});
-  response.end("Hello");
+  homeRoute(request, response);
 }).listen(3000);
-console.log('Server running at http//');
+console.log('Server running at http//<LouisvilleECD-url/');
 
+
+function homeRoute(request, response) {
+  if(request.url === "/") {
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end("Footer\n");
+}
+}
+
+function userRoute(request, response) {
+  var username = request.url.replace("/", "");
+  if(username.length > 0) {
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.write("Header\n");
+    response.write(username + "\n");
+    response.end("Footer\n");
+  }
+}
 //function printMessage(username) {
 //  var message = "Welcome, " + username + ". Let's dance!";
 //  console.log(message); }
