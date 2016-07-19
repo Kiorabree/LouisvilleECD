734f0d99595ac3db4db88b28c44c917acb83bde7
Kiorabree <rharper01@bellarmine.edu>
2016-07-19 17:24:34 -0400

    Working on more node

--- router.js
+++ router.js
@@ -1,35 +1,38 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
 
+var commonHeaders = {"Content-Type": "text/html"}
 function homeRoute(request, response) {
   if(request.url === "/") {
-    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.writeHead(200, commonHeaders);
     renderer.view("header", {}, response);
-    response.write("Search\n");
-    response.end("Footer\n");
+    renderer.view("login", {}, response);
+    renderer.view("footer", {}, response);
+    response.end();
 }
 }
 
 function userRoute(request, response) {
   var username = request.url.replace("/", "");
   if(username.length > 0) {
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.write("Header\n");
+    response.writeHead(200, commonHeaders);
+    renderer.view ("header", {}, response);
 
     var dancerProfile = new Profile(username);
 
     dancerProfile.on("end", function(ECDprofile){
-      var values = {
-        username: profileJSON.gravatar_url
-      }
+      var values = {  username: profileJSON.gravatar_url  }
 
-      response.write(values.username + "\n");
-      response.end("Footer\n");
-
+      renderer.view("profile", values, response);
+      renderer.view("footer", {}, response);
+      response.end();
     });
 
     dancerProfile.on("error", function(error){
-      response.end("Footer\n");
+      renderer.view("error", {errorMessage: error.message}, response);
+      renderer.view("login", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
     });
   }
 }
