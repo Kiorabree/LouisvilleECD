c8a12fd81978acfd7a3f414f2db1216f010df3dd
Kiorabree <rharper01@bellarmine.edu>
2016-07-20 22:08:07 -0400

    Organization changes

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-var router = require("./router.js");
+'use strict';
 
-var http = require("http");
-http.createServer(function (request, response) {
-  router.home(request, response);
-  router.user(request, response);
-}).listen(3000);
-console.log('Server running at http//<LouisvilleECD-url/');
+var express = require('express'),
+    posts = require('.json');
+
+var app = express();
+
+app.get('/', function(request, response){
+  response.send()
+});
+app.get('/blog/:username?', function(request, response){
+  var username = request.params.username;
+  if  (username === undefined) {
+    response.status(503);
+    response.send('was not found.')
+  } else {
+      var post = posts[username];
+      response.send(post);
+  }
+})
+app.listin(3000, function(){
+
+});
+//var router = require("./router.js");
+
+//var http = require("http");
+//http.createServer(function (request, response) {
+//  router.home(request, response);
+//  router.user(request, response);
+//}).listen(3000);
+//console.log('Server running at http//<LouisvilleECD-url/');
 
 //function printMessage(username) {
 //  var message = "Welcome, " + username + ". Let's dance!";
