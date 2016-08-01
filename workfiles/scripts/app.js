88a45e0b5108ca0ced7e189e20f66c754a3b531f
Kiorabree <rharper01@bellarmine.edu>
2016-07-31 23:57:44 -0400

    Trying one more time to git this to work

--- workfiles/scripts/app.js
+++
@@ -1,6 +0,0 @@
-angular.module("dancersApp", [])
-.controller('dancersList', function($scope) {
-  $scope.helloWorld = function() {
-    console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
-  };
-});
